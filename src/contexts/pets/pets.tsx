0d9e21230a205d4af062253e972e35c698085510
Brenno Luan <brennoluan8530@gmail.com>
2026-05-20 17:38:06 -0300

    Migrate `users` and `pets` contexts to TypeScript and add type annotations

---
+++ src/contexts/pets/pets.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import {createContext, useContext, useEffect, useState} from 'react';
+import type {Pet} from "../../types";
+import {fetchData} from "../../utils";
+
+const PetsContext = createContext<{ pets: Pet[] }>({pets: []});
+
+export const PetsProvider = ({children}: { children: React.ReactNode }) => {
+    const [pets, setPets] = useState<Pet[]>([]);
+
+    const fetchPets = async () => {
+        const data = await fetchData<Pet[]>('http://localhost:3001/pets');
+        setPets(data);
+    };
+
+    useEffect(() => {
+        fetchPets();
+    }, []);
+
+    return <PetsContext.Provider value={{pets}}>{children}</PetsContext.Provider>;
+};
+
+export const usePets = () => {
+    const context = useContext(PetsContext);
+    if (!context) {
+        throw new Error('usePets must be used within a PetsProvider');
+    }
+    return context;
+};
---
+++ src/contexts/users/users.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import {createContext, useContext} from 'react';
+import authStorage from 'simple-auth-storage';
+import type {User, UserLogin, UserRegister} from "../../types";
+
+const UsersContext = createContext<{
+    createUser: (user: UserRegister) => Promise<void>;
+    validateUser: (userLogin: UserLogin) => Promise<User | null>;
+    updateUser: (user: User) => Promise<void>;
+    getUser: () => User | null;
+    isAuthenticated: () => boolean;
+    login: (userLogin: UserLogin) => Promise<User | null>;
+}>({
+    createUser: async () => {
+    },
+    validateUser: async () => null,
+    updateUser: async () => {
+    },
+    getUser: () => null,
+    isAuthenticated: () => false,
+    login: async () => null,
+});
+
+export const UsersProvider = ({children}: { children: React.ReactNode }) => {
+    const fetchUsers = async () => {
+        const response = await fetch('http://localhost:3001/users');
+        const data = await response.json();
+        return data;
+    };
+
+    const createUser = async (user: UserRegister) => {
+        const checkForDuplicate = await fetchUsers();
+        const isDuplicate = checkForDuplicate.some((u: User) => u.email === user.email);
+        if (isDuplicate) {
+            throw new Error('User already exists');
+        }
+        const response = await fetch('http://localhost:3001/users', {
+            method: 'POST',
+            body: JSON.stringify(user),
+        });
+        const data = await response.json();
+        const token = authStorage.generateToken(data);
+        authStorage.saveUser(data, token);
+        return data;
+    };
+
+    const validateUser = async (userLogin: UserLogin) => {
+        const users = await fetchUsers();
+        const userFound = users.find((user: UserLogin) => user.email === userLogin.email && user.password === userLogin.password);
+        if (userFound) {
+            return userFound;
+        }
+        return null;
+    };
+
+    const updateUser = async (user: User) => {
+        const response = await fetch(`http://localhost:3001/users/${user.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+        });
+        const data = await response.json();
+        const token = authStorage.generateToken();
+        authStorage.saveUser(data, token);
+        return data;
+    };
+
+    const login = async (UserLogin: UserLogin) => {
+        const user = await validateUser(UserLogin);
+        if (user) {
+            const token = authStorage.generateToken(user);
+            authStorage.saveUser(user, token);
+        }
+        return user;
+    };
+
+    const getUser = () => {
+        const user = authStorage.getUser();
+        return user;
+    };
+
+    const isAuthenticated = () => {
+        const isLoggedIn = authStorage.isLoggedIn();
+        return isLoggedIn;
+    };
+
+    return (
+        <UsersContext.Provider
+            value={{createUser, validateUser, updateUser, getUser, login, isAuthenticated}}
+        >
+            {children}
+        </UsersContext.Provider>
+    );
+};
+
+export const useUsers = () => {
+    const context = useContext(UsersContext);
+    if (!context) {
+        throw new Error('useUsers must be used within a UsersProvider');
+    }
+    return context;
+};
