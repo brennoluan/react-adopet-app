5ad7182421be5d9ef3e9dae1c35e681d5d993533
Brenno Luan <brennoluan8530@gmail.com>
2026-05-20 16:42:51 -0300

    Add User interface to type definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,4 +8,11 @@ export interface Pet {
     image?: string | null,
 }
 
-export type User = {};
+export interface User {
+    id: string,
+    name: string,
+    email: string,
+    phone?: string,
+    city?: string,
+    about?: string,
+}
